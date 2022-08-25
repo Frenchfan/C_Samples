5aaf57226bebf47177ee9d602b2ec7ad20433e2c
Frenchfan <sumkin.gpb@gmail.com>
2022-08-25 12:47:20 +0300

    Renaming and structuring

---
+++ Seminar0100/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Результат = {0}", sqr);// 0 - Индекс переменной, указанной после текста через запятую 
+Console.WriteLine($"Результат = {sqr}"); // предпочтительный вариант.$ - показывает, что есть переменная
+
+int result = Convert.ToInt32(Math.Pow(number, 2));// самый правильный вариант, поскольку Pow выдает результат типа double
+Console.WriteLine($"Результат = {result}");
---
+++ Seminar0103Days/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string dayofweek = int.Parse(Console.ReadLine()) switch{
+    1 => "Понедельник",
+    2 => "Вторник",
+    3 => "Среда",
+    4 => "Четверг",
+    5 => "Пятница",
+    6 => "Суббота",
+    7 => "Воскресенье",
+    _=> "Неверно указан день"
+};
+Console.WriteLine(dayofweek);
