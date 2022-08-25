ce97cabc38ecaf96e0425f33ac27d3c5e775e12a
Frenchfan <sumkin.gpb@gmail.com>
2022-08-25 19:56:54 +0300

    Homework - Seminar 2, C#, Tasks 10, 13, 15

---
+++ Seminar02/Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите не менее, чем трехзначное число");
+int a = int.Parse(Console.ReadLine());
+if (a < 100) 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    Console.WriteLine($"Третья цифра числа {a} справа налево - {a / 100 % 10}");
+}
+
---
+++ Seminar02/Task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите номер дня недели, а я подскажу, выходной ли это: ");
+string dayofweek = int.Parse(Console.ReadLine()) switch{
+    > 0 and < 6 => "Рабочий день",    
+    >= 6 and <= 7 => "Да, это выходной, гоу отдыхать",    
+    _=> "Неверно указан день"
+};
+Console.WriteLine(dayofweek);
