ea7e197b6556ac05f4dd450452781fdd94be6c78
Frenchfan <sumkin.gpb@gmail.com>
2022-08-25 20:26:18 +0300

    Added task on defining x and y depending on the quarter (Decart model)

--- Seminar03/Task18/Program.cs
+++ Seminar03/Task18/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("Введите номер четверти: ");
-string quarter = int.Parse(Console.ReadLine()) switch{
+string quarter = int.Parse(Console.ReadLine()) 
+switch
+{
     1 => "x > 0, y > 0",
     2 => "x < 0, y > 0",
     3 => "x < 0, y < 0",
