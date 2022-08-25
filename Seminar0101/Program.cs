1cfa2872bfa4cc3c71a345f549cd708702831958
Frenchfan <sumkin.gpb@gmail.com>
2022-08-25 12:45:17 +0300

    Renaming and structuring

--- Seminar0101/Program.cs
+++ Seminar0101/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число (проверим, является ли оно квадратным корнем из первого): ");
+int b = int.Parse(Console.ReadLine());
+bool flag = (b * b == a);
+if (flag) 
+{
+    Console.WriteLine($"Первое число {a} является квадратом второго числа {b}");
+}
+else 
+{
+    Console.WriteLine($"Первое число {a} НЕ является квадратом второго числа {b}");
+
+}
