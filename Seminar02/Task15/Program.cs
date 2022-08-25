1cf0c1796b3133eb62929d3cafc22c1183a50eeb
Frenchfan <sumkin.gpb@gmail.com>
2022-08-25 20:07:31 +0300

    Homework - Seminar 2, C#, Tasks 10, 13, 15

--- Seminar02/Task15/Program.cs
+++ Seminar02/Task15/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите номер дня недели, а я подскажу, выходной ли это: ");
-int dayofweek = int.Parse(Console.ReadLine()) switch{
+string dayofweek = int.Parse(Console.ReadLine()) switch{
     > 0 and < 6 => "Рабочий день",    
     >= 6 and <= 7 => "Да, это выходной, гоу отдыхать",    
     _=> "Неверно указан день"
